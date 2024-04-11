2e5b489bd57f7d9f999d8f68dcf330cc909bd9be
Sina Najafi <sina@Ubuntu.myguest.virtualbox.org>
2024-04-11 13:19:30 -0700

    changed

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
 pub fn twofer(name: &str) -> String {
-    unimplemented!("how many for {name}")
+    let sn = "One for you, one for me.";
+    let snt = "";
+    if name == snt {
+        return sn.to_string()
+    } else {
+        format!("One for {}, one for me.", name).to_string()
+    }
 }
+
